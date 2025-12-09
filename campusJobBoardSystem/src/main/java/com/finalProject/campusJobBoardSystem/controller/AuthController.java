7d0cfa362edbfdfb83ff3ecfa34d38f2d7873b99
thewhoshirt <s.embury.36@gmail.com>
2025-12-09 14:36:42 -0700

    AuthController update to register 'views'

--- campusJobBoardSystem/src/main/java/com/finalProject/campusJobBoardSystem/controller/AuthController.java
+++ campusJobBoardSystem/src/main/java/com/finalProject/campusJobBoardSystem/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.finalProject.campusJobBoardSystem.controller;
 
+import com.finalProject.campusJobBoardSystem.model.User;
 import com.finalProject.campusJobBoardSystem.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -10,31 +11,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
-//    private final UserRepository repo;
-//    private final BCryptPasswordEncoder encoder;
-//
-//    public AuthController(UserRepository repo, BCryptPasswordEncoder encoder) {
-//        this.repo = repo;
-//        this.encoder = encoder;
-//    }
-//
-//    @GetMapping("/login")
-//    public String login(){
-//        return "login";
-//    }
-//
-//    @GetMapping("/register")
-//    public String register(Model model){
-//        model.addAttribute("user", new User());
-//        return "register";
-//    }
-//
-//    @PostMapping("/register")
-//    public String registerUser(@ModelAttribute User user){
-//        user.setPassword(encoder.encode(user.getPassword()));
-//        user.setRole("ROLE_USER");
-//        user.setEnabled(true);
-//        repo.save(user);
-//        return "redirect:/login?registered";
-//    }
+    private final UserRepository repo;
+    private final BCryptPasswordEncoder encoder;
+
+    public AuthController(UserRepository repo, BCryptPasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/studentRegister")
+    public String registerStudent(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @GetMapping("/employerRegister")
+    public String registerEmployer(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/studentRegister")
+    public String studentRegister(@ModelAttribute User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole(User.Role.STUDENT);
+        user.setStatus(User.Status.ACTIVE);
+        repo.save(user);
+        return "redirect:/login?registered";
+    }
+    @PostMapping("/employerRegister")
+    public String employerRegister(@ModelAttribute User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole(User.Role.EMPLOYER);
+        user.setStatus(User.Status.ACTIVE);
+        repo.save(user);
+        return "redirect:/login?registered";
+    }
 }
