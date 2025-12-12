1772541cd6569455eb30ab60e3c5deb307af8dd2
thewhoshirt <s.embury.36@gmail.com>
2025-12-12 15:55:31 -0700

    start of testing for controller

---
+++ campusJobBoardSystem/src/test/java/com/finalProject/campusJobBoardSystem/controller/AdminControllerTest.java
@@ -0,0 +1,34 @@
+package com.finalProject.campusJobBoardSystem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class AdminControllerTest {
+
+    @Test
+    void users() {
+    }
+
+    @Test
+    void activateUser() {
+    }
+
+    @Test
+    void deactivateUser() {
+    }
+
+    @Test
+    void jobs() {
+    }
+
+    @Test
+    void approveJob() {
+    }
+
+    @Test
+    void rejectJob() {
+    }
+}
---
+++ campusJobBoardSystem/src/test/java/com/finalProject/campusJobBoardSystem/controller/AuthControllerTest.java
@@ -0,0 +1,26 @@
+package com.finalProject.campusJobBoardSystem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+
+    @Test
+    void home() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void testRegister() {
+    }
+}
