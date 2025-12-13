6089bf779ac0659b86b60ea60d42e24e9a307711
thewhoshirt <s.embury.36@gmail.com>
2025-12-12 17:03:37 -0700

    start of testing for controller mokito not working

--- campusJobBoardSystem/src/test/java/com/finalProject/campusJobBoardSystem/controller/StudentControllerTest.java
+++ campusJobBoardSystem/src/test/java/com/finalProject/campusJobBoardSystem/controller/StudentControllerTest.java
@@ -3,14 +3,14 @@ package com.finalProject.campusJobBoardSystem.controller;
 import com.finalProject.campusJobBoardSystem.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(StudentController.class)
-@ExtendWith(MockitoExtension.class)
 class StudentControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -19,6 +19,7 @@ class StudentControllerTest {
 
     @Test
     void jobs() {
+        Mockito.when(service.findAll()).thenReturn(null);
     }
 
     @Test
